3b228ff8500421ef741e66553fff1c14e63d5f97
shinyay <shinya.com@gmail.com>
2024-05-17 14:44:39 +0900

    Add spring template with SpringBootCLI

---
+++ workspace/src/test/java/com/example/aicompletiondemo/AiCompletionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicompletiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCompletionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
