301fff0349f60744801079f34017b82719dc67da
shinyay <shinya.com@gmail.com>
2024-05-29 17:55:32 +0900

    Modify package and initialization on ChatClient

--- workspace/src/main/java/com/microsoft/shinyay/ai/SimpleAiController.java
+++ workspace/src/main/java/com/microsoft/shinyay/ai/SimpleAiController.java
@@ -1,6 +1,6 @@
 package com.microsoft.shinyay.ai;
 
-import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +10,13 @@ public class SimpleAiController {
 
     private final ChatClient chatClient;
 
-    public SimpleAiController(ChatClient chatClient) {
-        this.chatClient = chatClient;
+    public SimpleAiController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
     }
 
     @GetMapping("/ai/recipe")
-    public String getRecipe(@RequestParam(value = "userInput", defaultValue = "Tell me a delicious food recipe") String userInput) {
+    public String getRecipe(
+            @RequestParam(value = "userInput", defaultValue = "Tell me a delicious food recipe") String userInput) {
         return chatClient.prompt()
                 .user(userInput)
                 .call()
