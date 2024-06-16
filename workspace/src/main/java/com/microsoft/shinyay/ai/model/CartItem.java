b778251c612f4adb6796e1ef9969ef683a36a093
Shinya Yanagihara <shinyay@github.com>
2024-06-16 13:33:52 +0900

    Add shopping cart functionality

---
+++ workspace/src/main/java/com/microsoft/shinyay/ai/model/CartItem.java
@@ -0,0 +1,25 @@
+package com.microsoft.shinyay.ai.model;
+
+public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public CartItem() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
